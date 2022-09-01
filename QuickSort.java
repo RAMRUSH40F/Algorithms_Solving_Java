a2eaa82bc6484e6c76a3f3ac0fde81a3639e62ae
ramru <ramrush-r@mail.ru>
2022-09-01 14:36:34 +0300

    Updated

--- QuickSort.java
+++ QuickSort.java
@@ -1,61 +1,73 @@
-import java.util.Arrays;
-
 public class QuickSort
 {
 
-    public static void quickSort(int[] array, int low, int high) {
-        if (array.length == 0)
-            return;//завершить выполнение, если длина массива равна 0
+    public static void quickSort(int[] array, int low, int high)
+    {
+        if ((array.length==0) || (low>=high))
+            return;// Если длина массива 0,если уже нечего делить, остановить.
 
-        if (low >= high)
-            return;//завершить выполнение если уже нечего делить
+        int mid =low + (high - low)/2;
+        int pivot = array[mid]; // Выбираем опорный элемент(пивот) как серединный, хоть, как я слышал, есть усовершенствованная версия;
 
-        // выбрать опорный элемент как серединный.
-        int pivot = array[low + (high - low) / 2];
-
-        // разделить на подмассивы, который больше и меньше опорного элемента
         int i = low, j = high;
-        while (i <= j) {
+
+        while (i<j) {
+
             while (array[i] < pivot) {
                 i++;
             }
-
             while (array[j] > pivot) {
                 j--;
             }
 
-            if (i <= j) {//меняем местами
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
-            }
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            i++;
+            j--;
         }
+        quickSort(array, low, mid);
+        quickSort(array,mid+1, high);
 
-        // вызов рекурсии для сортировки левой и правой части
-        if (low < j)
-            quickSort(array, low, j);
-
-        if (high > i)
-            quickSort(array, i, high);
     }
 
     public static void main(String[] args) {
 
-        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
-
-        System.out.println("Было");
-        System.out.println(Arrays.toString(x));
-
-        int low = 0;
-        int high = x.length - 1;
-
-        quickSort(x, low, high);
-        System.out.println("Стало");
-        System.out.println(Arrays.toString(x));
+//        int[] x = { 8, 0, 4, 7, 3, 7, 10, 12, -3 };
+//        System.out.println(Arrays.toString(x));
+//
+//        int low = 0;
+//        int high = x.length - 1;
+//
+//        quickSort(x, low, high);
+//
+//        System.out.println(Arrays.toString(x));
+        test1();
     }
 
 
+    private static void test1() {
+        int testLen = 10000000;
+
+        int[] arr1 = new int[testLen];
+        int[] arr2 = new int[testLen];
+
+        System.out.println("\n-----Случайный массив------");
+
+        for (int i = 0; i < testLen; i++) {
+            arr2[i] = arr1[i] = (int)Math.round(Math.random() * 10000);
+        }
+        
+
+        System.out.println("Быстрая сортировка:");
+        measureTime(() -> quickSort(arr1, 0, testLen - 1));
+    }
+
+    private static void measureTime(Runnable task) {
+        long startTime = System.currentTimeMillis();
+        task.run();
+        long elapsed = System.currentTimeMillis() - startTime;
+        System.out.println("Затраченное время: " + elapsed + " ms");
+    }
 
 }
